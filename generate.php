087e4db9fb789432ad8755f2e283699ab32c758b
mike <mike@whatsdao.org>
2022-04-12 10:15:04 +0800

    change

--- generate.php
+++ generate.php
@@ -1,9 +1,19 @@
 <?php
 
-for($i = 0; $i < 3; $i ++ ) {
+for($i = 1; $i < 3; $i ++ ) {
 	$metadata = 
-		'{"name": "Meta-Intel Pentium #'.$i.'","image": "", "attributes": [{"trait_type": "Version", "value": "M-1"}, {"trait_type": "Hashrate", "value": "100"}, {"trait_type": "Consumption", "value": "10"}]}';
-	$file = "./". $i;
+		'{
+    "name": "Meta-Intel Pentium 4 #'. $i .'",
+    "description": "MBTC M-1 series miner",
+    "image": "https://api.meta-btc.org/nft/images/metaintelp4.png",
+    "image_4K": "https://api.meta-btc.org/nft/images/metaintelp4_hd.png",
+    "attributes": {
+        "Version": "M-1",
+        "Hashrate": "100",
+        "Consumption": "10"
+    }
+}';
+	$file = "./". $i ;
 	file_put_contents($file, $metadata);
 
 }
